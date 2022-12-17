f1b13c4d464de1ade887640b4ee5668dc7a98318
Lakshmaji Mutyala <lakshmaji@users.noreply.github.com>
2022-12-17 17:25:41 +0530

    feat: :art: use types (#2)

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # Rails and React boilerplate 🎉
 
-This is a starter for rails and react monolith application. You can find instruction [here](https://lakshmaji.medium.com/rails-7-reactjs-setup-monolith-b9baa0f84271) if you want to setup your own.
+This is a starter kit template for rails and react monolith application. You can find instruction [here](https://lakshmaji.medium.com/rails-7-reactjs-setup-monolith-b9baa0f84271) if you want to setup your own.
+
+## What's inside
+
+This a starter kit template to get started with rails and react. This will renders react component in client side. i.e, browser.
+
+1. Rails 7
+2. React 18
+3. Inertia 0.11.1
+
+## Setup Instructions
+
+1. Clone repository
+2. Install ruby dependencies
+
+    ```bash
+    bundle install
+    ```
+
+3. Install npm dependencies
+
+    ```bash
+    yarn install
+    ```
+
+### Development
+
+1. Launch rails (puma) server
+
+    ```bash
+    bin/rails server
+    ```
+
+2. Serve react components
+
+    ```bash
+    bin/vite dev
+    ```
--- app/javascript/components/Application.tsx
+++ app/javascript/components/Application.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createInertiaApp } from "@inertiajs/inertia-react";
+import {
+  createInertiaApp,
+  InertiaAppOptionsForCSR,
+} from "@inertiajs/inertia-react";
 
-createInertiaApp({
-  resolve: async (name) => {
-    return import(`./${name}`);
-  },
-  setup({ el, App, props }) {
-    const container = document.getElementById(el.id);
-    const root = createRoot(container!);
-    root.render(<App {...props} />);
-  },
+const pages = import.meta.glob("./*.tsx");
+
+const app = () =>
+  createInertiaApp<InertiaAppOptionsForCSR<unknown>>({
+    resolve: async (name) => {
+      const module = await pages[`./${name}.tsx`]();
+      const page = (
+        module as never as { default: { layout: React.ReactFragment } }
+      ).default;
+      return page;
+    },
+    setup({ el, App, props }) {
+      const container = document.getElementById(el.id);
+      const root = createRoot(container!);
+      root.render(<App {...props} />);
+    },
+  });
+
+document.addEventListener('DOMContentLoaded', () => {
+  app();
 });
+  
